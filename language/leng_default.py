d9125d31db9da653ccf9da7de5df11fc0c2c5a53
neo-jack-official <59382524+neo-jack-official@users.noreply.github.com>
2020-06-17 22:12:13 +0000

    u
    u

---
+++ language/leng_default.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#By Neo-Jack
+
+#El archivo fuente "leng_default.py", debe mantener el mismo nombre.
+#Para cambiar el idioma, cambie el nombre del archivo de idioma a "leng_default.py"
+#Ej. "leng_es.py"
+#Renombrarlo a "leng_default.py"
+
+#Instrucciones:
+#Considere al editar manualmente. En las primeras 10 líneas.
+#Si se cambian las variables, debe contener el mismo número de caracteres.
+#Si le faltan caracteres, agregue ESPACIOS, si le faltan caracteres, elimine ESPACIOS.
+#NO EXCEDA el número TOTAL de caracteres.
+# --> significa a la derecha.
+# <-- significa a la izquierda.
+#
+#Ex: "Puerto: " #Total de caracteres: 8, -->: 1 ESPACIO.  (palabra = 7, espacio = 1, palabra + espacios = 8 Total de Caracteres)
+#    "Port:   " #Total de characteres: 8, -->: 3 ESPACIOS. (palabra = 5, espacios = 3, palabra + espacios = 8 Total de Caracteres)
+
+parametros_defecto_leng = "Parametros por Defecto:                              " # Total: 53, -->: 30 
+asociado_puerto_leng = "   (Asociado al Puerto)          " # Total: 33, <--: 3, -->: 10
+obligatorio_leng = "   (OBLIGATORIO)                 " # Total: 33, <--: 3, -->: 17 
+web_leng = "Web o IP: " # Tota: 10, -->: 1 
+puerto_leng = "Puerto: " # Total: 8, -->: 1
+clientes_leng = "Clientes: " # Total: 10, -->: 1
+metodo_leng = "Metodo: " # Total: 8, -->: 1
+desactivado_leng = "     [Desactivado]                              " # Total: 48, <--: 5, -->: 30 
+or_leng = " o " # Total: 3, <--: 1, -->: 1
+para_salir_leng = "Para Salir" #Total: 10 
+#----------------------------------------------------------------------------------------
+#A partir de ahora, no importa la cantidad de caracteres, puedes cambiar lo que quieras.
+
+#A:
+activar_leng = "Activar"
+activado_leng = "Activado"
+adios_leng = "Demasiados intentos. ADIOS"
+asignando_nuevo_leng = "Asignando nuevo agente a cliente"
+#B:
+bots_verificadores_leng = "BOTS Verificadores"
+#C:
+cli_txt = "Cantidad de Clientes a usar?"
+cuantos_clientes_leng = "Cuantos Clientes deseas usar?"
+comprobando_info_leng = "Comprobando Informacion"
+comprobando_leng = "Comprobando"
+clientes2_leng = "Clientes"
+conectando_cliente_leng = "Conectando Cliente a"
+caido_protegido_leng = "Posiblemente Caido !!! o Protegido!!!"
+#D:
+desactivar_leng = "Desactivar"
+desactivado2_leng = "Desactivado"
+defecto_leng = "Defecto"
+#E:
+ej_leng = "Ej."
+ejemplo_leng = "ejemplo"
+estado_leng = "Estado"
+error500_leng = "Error 500! Servidor"
+#F:
+#G:
+#H:
+host_cerrado_leng = "Host Inexistente o Puerto Cerrado"
+#I:
+info_leng = "INFO"
+inexistente_leng = "Inexistente Web o IP"
+intentalo_leng = "Intentelo otra vez"
+iniciando_leng = "Iniciando"
+ip_asignada_leng = "IP Tor Asignada"
+ip_segura_leng = "IP-Segura"
+ip_expuesta_leng = "IP-Expuesta"
+imp_hand_leng = "Imposible handshake con el Servidor"
+#J:
+#K:
+#L:
+#M:
+me_txt = "Seleccionando Metodo a usar"
+metodos2_leng = "Metodos"
+metodo2_leng = "Metodo"
+#N:
+nota_leng = "NOTA"
+obligatorio_leng2 = "OBLIGATORIO"
+no_tor_instalado_leng = "Usted no tine Servidor TOR Instalado (Tor Expert Bundle)"
+nuevo_agente_leng = "Nuevo Agente Asignado"
+#O:
+o_leng = "o" #ingles: or
+opciones_leng = "Opciones"
+#P:
+puerto_a_usar_leng = "Puerto a usar"
+paquete_enviado_leng = "Paquete Enviado"
+paquete_no_enviado = "Paquete NO Entregado"
+para_terminar_leng = "Para Terminar"
+puerto2_leng = "Puerto"
+puerto_no_leng = "Puerto no Disponible en"
+#Q:
+quesitio_web_leng = "Que Sitio Web o Ip, usaremos?"
+que_puerto_leng = "Que Puerto quiere usar?"
+quiere_activar_tor_leng = "Quieres Activar TOR?"
+#R:
+revisar_pip_leng = "Revisaremos PIP, para instalar"
+revisar_tor_leng = "Revisaremos Tor Expert Bundle, para instalar"
+revisar_leng = "Revisaremos si falta alguna biblioteca, para instalar"
+respuesta_valida_leng = "Respuestas validas"
+recomendado_leng = "Recomendados"
+reconectando_leng = "Re-Conectando Cliente a"
+#S:
+seleccione_metodo_leng = "Seleccione el Nº del Metodo?"
+seguridad_ip_leng = "Seguridad de IP"
+servidor_ip_leng = "Servidor IP-Checker"
+seguro_continuar_leng = "Esta Seguro de Continuar?"
+s_leng = "s" # [s] --> SI, or [y] --> YES
+#T:
+tor_txt = "Servicio TOR Network?"
+target_desconosido_leng = "Target Desconocido"
+target_leng = "Target"
+#U:
+ultima_oportunidad_leng = "Ultima Oportunidad"
+use_vpn_leng = "SIEMPRE USE VPN, SU UBICACION PODRIA QUEDAR EXPUESTA"
+uagent_leng = "Agentes de Navegador"
+#V:
+ventana_cierra_leng = "Esta ventana se cerrara en (7) Seg."
+verificando_leng = "Verificando"
+verifique_leng = "Verifique la WEB o el IP"
+#W:
+web_ip = "Web o Ip a usar!!!"
+web_invalido_leng = "Web o IP invalido, Reintente"
+window = "Version para Python3"
+#X:
+#Y:
+#Z:
