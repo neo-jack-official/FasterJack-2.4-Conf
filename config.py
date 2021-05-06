ab4a1cde2537b9f53e50311c2e46b6571b0b0d77
neo-jack-official <59382524+neo-jack-official@users.noreply.github.com>
2021-05-06 22:23:48 +0000

    Update
    Update

--- config.py
+++ config.py
@@ -7,17 +7,17 @@
 #If you running FasterJack as .EXE. check_tor_conf = "no"
 
 # --- CONFIGURATION ---
-language_conf = "en"        #Select Language: Options --> [default], [es], [en], [pt]
+language_conf = "es"        #Select Language: Options --> [default], [es], [en], [pt]
 check_pip_config = "no"     #Check PIP to Install?: Options --> [yes] or [no]. only activate under Python 3
-check_libraries_conf = "yes" #Check libraries at Startup?: Options --> [yes] or [no]. only activate under python 3
-check_tor_conf = "yes"      #Check Tor Expert Bundle to Install?: Options --> [yes] or [no]. only activate under LINUX
+check_libraries_conf = "no" #Check libraries at Startup?: Options --> [yes] or [no]. only activate under python 3
+check_tor_conf = "no"      #Check Tor Expert Bundle to Install?: Options --> [yes] or [no]. only activate under LINUX
 #Default variables
 port_conf = "443"            #Select default Port: Options --> [80] or [443]
-clientes_conf = "70"        #Select default number of customers, recommended between [30 - 135]
-Tor_conf = "off"            #Select default Tor-Network: Options --> [on] or [off]
+clientes_conf = "135"        #Select default number of customers, recommended between [30 - 135]
+Tor_conf = "on"            #Select default Tor-Network: Options --> [on] or [off]
 
 #Malicious Package
-package_conf = "normal"    #Malicious package, directly influences your internet consumption : Options --> [basic] or [normal] or [strong]
+package_conf = "basic"    #Malicious package, directly influences your internet consumption : Options --> [basic] or [normal] or [strong]
 
 #Extra Notifications        (Off mode make work faster the Ddos)
 conectando_conf = "off"     #Select if you want to see connected notification: Options --> [on] or [off]
@@ -31,31 +31,37 @@ verif_host_conf = "off"      #select if you want to see host verification notifi
 
 ##From now on, Do not touch, if you do not know what you are doing.
 # --- Ip-Checker ---
-ipcheck_url1_conf = 'http://icanhazip.com'   # Service Ip-Checker 01 --> Do not modify, unless they fail.
-ipcheck_url2_conf = 'http://icanhazptr.com'  # Service Ip-Checker 02 --> Do not modify, unless they fail.
-ipcheck_url3_conf = 'https://ifconfig.me/ip' # Service Ip-Checker 03 --> Do not modify, unless they fail.
-ipcheck_url4_conf = 'https://ipinfo.io/ip'   # Service Ip-Checker 03 --> Do not modify, unless they fail.
-ipcheck_url5_conf = 'https://ipecho.net/plain'   # Service Ip-Checker 03 --> Do not modify, unless they fail.
-ipcheck_url6_conf = 'http://ident.me/'       # Service Ip-Checker 03 --> Do not modify, unless they fail.
-ipcheck_url7_conf = 'http://checkip.amazonaws.com/' # Service Ip-Checker 03 --> Do not modify, unless they fail.
+ipcheck_url1_conf = 'http://icanhazip.com'   # Service Ip-Checker 01 --> Do not modify, unless they fail.                check 06/may/2021
+ipcheck_url2_conf = 'http://bot.whatismyipaddress.com'  # Service Ip-Checker 02 --> Do not modify, unless they fail.     check 06/may/2021 
+ipcheck_url3_conf = 'https://ifconfig.me/ip' # Service Ip-Checker 03 --> Do not modify, unless they fail.                check 06/may/2021
+ipcheck_url4_conf = 'https://ipinfo.io/ip'   # Service Ip-Checker 03 --> Do not modify, unless they fail.                check 06/may/2021
+ipcheck_url5_conf = 'https://ipecho.net/plain'   # Service Ip-Checker 03 --> Do not modify, unless they fail.            check 06/may/2021
+ipcheck_url6_conf = 'http://ident.me/'       # Service Ip-Checker 03 --> Do not modify, unless they fail.                check 06/may/2021
+ipcheck_url7_conf = 'http://checkip.amazonaws.com/' # Service Ip-Checker 03 --> Do not modify, unless they fail.         check 06/may/2021
 # --- Bots ---
-bot_url1_conf = "https://html5.validator.nu/?doc="               # Service Bot 01 --> Do not modify, unless they fail.
+bot_url1_conf = "https://html5.validator.nu/?doc="               # Service Bot 01 --> Do not modify, unless they fail. 
 bot_url2_conf = "https://validator.w3.org/nu/?doc="              # Service Bot 02 --> Do not modify, unless they fail.
 bot_url3_conf = "https://datayze.com/site-validator.php?domain=" # Service Bot 03 --> Do not modify, unless they fail.
 bot_url4_conf = "https://validator.ampproject.org/#url="         # Service Bot 04 --> Do not modify, unless they fail.
-bot_url5_conf = "http://www.feedvalidator.org/check.cgi?url="    # Service Bot 05 --> Do not modify, unless they fail.
+bot_url5_conf = "https://validator.w3.org/check?uri="    # Service Bot 05 --> Do not modify, unless they fail.
 # --- Others ---
 headers_conf = "headers.txt"                 #Package Headers
 #---------------------------------
 
 # --- Gallery of Ip-Checkers -- if you need remplace it: ---
-# http://smart-ip.net/myip
-# http://bot.whatismyipaddress.com/
-# http://whatismyip.akamai.com/
-# http://ip.tyk.nu/
-# https://ipof.in/txt
-# http://tnx.nl/ip
-# http://l2.io/ip
-# http://wgetip.com/ 
-# http://eth0.me/
+# http://smart-ip.net/myip           Down     check 06/may/2021
+# http://bot.whatismyipaddress.com/  USED     check 06/may/2021
+# http://whatismyip.akamai.com/      UP       check 06/may/2021
+# http://ip.tyk.nu/                  UP       check 06/may/2021
+# https://ipof.in/txt                UP       check 06/may/2021
+# http://tnx.nl/ip                   DOWN     check 06/may/2021
+# http://l2.io/ip                    UP       check 06/may/2021
+# http://wgetip.com/                 UP       check 06/may/2021
+# http://eth0.me/                    UP       check 06/may/2021
+#http://icanhazptr.com               Problem  check 06/may/2021
+
+# --- Gallery of Web validator
+# http://www.feedvalidator.org/check.cgi?url=       DOWN  check 06/may/2021 
+# https://www.scam-detector.com/validator/          DOWN  check 06/may/2021
+# https://validator.nu/?doc=                        UP    check 06/may/2021
 
